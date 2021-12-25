46e15263174b66bff26af091833c2c6c7f7c1bc5
mrahmani <mrahmani@student.42.fr>
2021-12-25 21:35:21 +0100

    norme cd.c

--- cd.c
+++ cd.c
@@ -12,69 +12,69 @@
 
 #include "minishell.h"
 
-char *get_path(char *arg)
+char	*get_path(char *arg)
 {
-    char *path;
+	char	*path;
 
-    if (ft_strncmp(arg, "~/", 2) == 0)
-        path = ft_substr(arg, 2, (ft_strlen(arg) - 2));
-    else
-        path = ft_strdup(arg);
-    return (path);
+	if (ft_strncmp(arg, "~/", 2) == 0)
+		path = ft_substr(arg, 2, (ft_strlen(arg) - 2));
+	else
+		path = ft_strdup(arg);
+	return (path);
 }
 
-int home_path()
+int	home_path(void)
 {
-    char *default_dir;
-    int ret;
+	char	*default_dir;
+	int		ret;
 
-    default_dir = ft_strjoin("/home/", getenv("USER"));
-    ret = chdir(default_dir);
-    if (default_dir != NULL)
-        free(default_dir);
-    return (ret);
+	default_dir = ft_strjoin("/home/", getenv("USER"));
+	ret = chdir(default_dir);
+	if (default_dir != NULL)
+		free(default_dir);
+	return (ret);
 }
 
-int change_path(char *path)
+int	change_path(char *path)
 {
-    int ret;
-    DIR *d;
+	int		ret;
+	DIR		*d;
 
-    d = opendir(path);
-    if (d == NULL)
-    {
-        printf("cd: %s: No such file or directory\n", path);
-        return (1);
-    }
-    ret = chdir(path);
-    if (ret < 0)
-    {
-        perror("cd ");
-        closedir(d);
-        return (1);
-    }
-    closedir(d);
-    return (0);
+	d = opendir(path);
+	if (d == NULL)
+	{
+		printf("cd: %s: No such file or directory\n", path);
+		return (1);
+	}
+	ret = chdir(path);
+	if (ret < 0)
+	{
+		perror("cd ");
+		closedir(d);
+		return (1);
+	}
+	closedir(d);
+	return (0);
 }
 
-int ft_cd(char **arg)
+int	ft_cd(char **arg)
 {
-    char *path;
-    int ret;
+	char	*path;
+	int		ret;
 
-    ret = 0;
-    if (arg[1] != NULL && arg[2] != NULL)
-    {
-        printf("cd: too many arguments\n");
-        return (1);
-    }
-    if (arg[1] == NULL || (ft_strcompare(arg[1], "~") == 1))
-        return (ret = home_path());
-    path = get_path(arg[1]);
-    ret = change_path(path);
-    if (path != NULL)
-        free(path);
-    else
-        ret = 1;
-    return (ret);
+	ret = 0;
+	if (arg[1] != NULL && arg[2] != NULL)
+	{
+		printf("cd: too many arguments\n");
+		return (1);
+	}
+	if (arg[1] == NULL || (ft_strcompare(arg[1], "~") == 1))
+		return (ret = home_path());
+	path = get_path(arg[1]);
+	ret = change_path(path);
+	if (path != NULL)
+		free(path);
+	else
+		ret = 1;
+	return (ret);
 }
