186bb3298f69a5b8828ac4f1a4343c1ba9f4b61b
mrahmani <mrahmani@student.42.fr>
2021-12-25 21:52:46 +0100

    norme check.c

--- check.c
+++ check.c
@@ -6,46 +6,46 @@
 /*   By: mrahmani <mrahmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 15:33:55 by ybesbes           #+#    #+#             */
-/*   Updated: 2021/12/20 16:31:12 by mrahmani         ###   ########.fr       */
+/*   Updated: 2021/12/25 21:51:54 by mrahmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int check_quotes(char *str, int *index, int *single_quotes, int *double_quotes)
+int	check_quotes(char *str, int *i, int *single_quotes, int *double_quotes)
 {
-	while (str != NULL && str[*index] != '\0')
+	while (str != NULL && str[*i] != '\0')
 	{
-		if (str[*index] == '\"' && (*index == 0 || str[*index - 1] != '\\'))
+		if (str[*i] == '\"' && (*i == 0 || str[*i - 1] != '\\'))
 		{
 			*double_quotes += 1;
-			*index += 1;
-			while (str[*index] != '\0' && (str[*index] != '\"' || str[*index - 1] == '\\'))
-				*index += 1;
-			if (str[*index] == '\0')
+			*i += 1;
+			while (str[*i] != '\0' && (str[*i] != '\"' || str[*i - 1] == '\\'))
+					*i += 1;
+			if (str[*i] == '\0')
 				return (1);
 			*double_quotes += 1;
 		}
-		if (str[*index] == '\'' && (*index == 0 || str[*index - 1] != '\\'))
+		if (str[*i] == '\'' && (*i == 0 || str[*i - 1] != '\\'))
 		{
 			*single_quotes += 1;
-			*index += 1;
-			while (str[*index] != '\0' && (str[*index] != '\''|| str[*index - 1] == '\\'))
-				*index += 1;
-			if (str[*index] == '\0')
+			*i += 1;
+			while (str[*i] != '\0' && (str[*i] != '\'' || str[*i - 1] == '\\'))
+				*i += 1;
+			if (str[*i] == '\0')
 				return (1);
 			*single_quotes += 1;
 		}
-		*index += 1;
+		*i += 1;
 	}
 	return (0);
 }
 
-int check_inclosed_quotes(char *str)
+int	check_inclosed_quotes(char *str)
 {
-	int i;
-	int quotes_num;
-	int double_quotes_num;
+	int		i;
+	int		quotes_num;
+	int		double_quotes_num;
 
 	i = 0;
 	double_quotes_num = 0;
@@ -57,31 +57,32 @@ int check_inclosed_quotes(char *str)
 	return (0);
 }
 
-int check_syntax_errors(char *str)
+int	check_syntax_errors(char *str)
 {
-	int i;
+	int		i;
 
 	i = ft_strlen(str);
 	if (check_inclosed_quotes(str) == 1)
 		return (1);
 	if (str != NULL && str[0] == '|')
 		return (1);
-	if (str != NULL && i > 0 && (str[i - 1] == '|' || str[i - 1] == '<' || str[i - 1] == '>'))
+	if (str != NULL && i > 0 && (str[i - 1] == '|'
+			|| str[i - 1] == '<' || str[i - 1] == '>'))
 		return (1);
 	return (0);
 }
 
-char *read_check_and_trim(t_shellinfo shell)
+char	*read_check_and_trim(t_shellinfo shell)
 {
-	char *str;
-	char *tmp;
+	char	*str;
+	char	*tmp;
 
 	str = readline("minishell->");
 	add_history(str);
 	if (str == NULL)
 	{
 		printf("\n");
-		ft_exit(shell, 0); // ctrl_d
+		ft_exit(shell, 0);
 	}
 	tmp = ft_strtrim(str, " \t\r\f\v\n");
 	free(str);
