d4c95be05225ac3573f3a2eba8b36c5356d0be62
mrahmani <mrahmani@student.42.fr>
2021-12-25 22:12:23 +0100

    norme echo.c

--- echo.c
+++ echo.c
@@ -12,28 +12,28 @@
 
 #include "minishell.h"
 
-int ft_echo(char **arg)
+int	ft_echo(char **arg)
 {
-    int i;
-    int has_option;
+	int		i;
+	int		has_option;
 
-    has_option = 0;
-    i = 1;
-    if (arg[1] == NULL)
-        return (printf("\n"));
-    if (ft_strcompare(arg[i], "-n") == 1 && i == 1)
-    {
-        i++;
-        has_option = 1;
-    }
-    while (arg && arg[i] != NULL)
-    {
-        ft_putstr_fd(arg[i], 1);
-        if (arg[i + 1] != NULL)
-            ft_putstr_fd(" ", 1);
-        i++;
-    }
-    if (has_option == 0)
-          ft_putstr_fd("\n", 1);
-    return (0);
+	has_option = 0;
+	i = 1;
+	if (arg[1] == NULL)
+		return (printf("\n"));
+	if (ft_strcompare(arg[i], "-n") == 1 && i == 1)
+	{
+		i++;
+		has_option = 1;
+	}
+	while (arg && arg[i] != NULL)
+	{
+		ft_putstr_fd(arg[i], 1);
+		if (arg[i + 1] != NULL)
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (has_option == 0)
+		ft_putstr_fd("\n", 1);
+	return (0);
 }
