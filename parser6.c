6672d8fbfd10c4089434842566787fbc0a18aad4
ybesbes <ybesbes@student.42.fr>
2021-12-26 14:40:33 +0100

    parser6.c done

--- parser6.c
+++ parser6.c
@@ -12,22 +12,23 @@
 
 #include "include/minishell.h"
 
-void is_double_quotes(t_command com, int *i, int *j, char **tab)
+void	is_double_quotes(t_command com, int *i, int *j, char **tab)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = *i;
 	*i += 1;
-	while (com.args[*i] != '\0' && (com.args[*i] != '\"' || com.args[*i - 1] == '\\'))
+	while (com.args[*i] != '\0' && (com.args[*i] != '\"'
+			|| com.args[*i - 1] == '\\'))
 		*i += 1;
 	tab[*j] = ft_substr(com.args, tmp, *i - tmp + 1);
 	*j += 1;
 	*i += 1;
 }
 
-void is_single_quotes(t_command com, int *i, int *j, char **tab)
+void	is_single_quotes(t_command com, int *i, int *j, char **tab)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = *i;
 	*i += 1;
@@ -38,22 +39,23 @@ void is_single_quotes(t_command com, int *i, int *j, char **tab)
 	*i += 1;
 }
 
-void is_char(t_command com, int *i, int *j, char **tab)
+void	is_char(t_command com, int *i, int *j, char **tab)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = *i;
-	while ((ft_isspace(com.args[*i]) == 0 && com.args[*i] != '\0') || is_it_between_quotes(com.args, *i) == 1)
+	while ((ft_isspace(com.args[*i]) == 0 && com.args[*i] != '\0')
+		|| is_it_between_quotes(com.args, *i) == 1)
 		*i += 1;
 	tab[*j] = ft_substr(com.args, tmp, *i - tmp);
 	*j += 1;
 }
 
-char **create_tab(t_command com, t_shellinfo shell)
+char	**create_tab(t_command com, t_shellinfo shell)
 {
-	char **tab;
-	int i;
-	int j;
+	char	**tab;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
@@ -62,12 +64,11 @@ char **create_tab(t_command com, t_shellinfo shell)
 		return (NULL);
 	while (com.args[i] != '\0')
 	{
-		if (com.args[i] == '\"' && (i == 0 || com.args[i - 1] != '\\')) // si double cote
+		if (com.args[i] == '\"' && (i == 0 || com.args[i - 1] != '\\'))
 			is_double_quotes(com, &i, &j, tab);
-		else if (com.args[i] == '\'') // si cote
+		else if (com.args[i] == '\'')
 			is_single_quotes(com, &i, &j, tab);
-	
-		else if (ft_isspace(com.args[i]) == 0) // si caractere
+		else if (ft_isspace(com.args[i]) == 0)
 			is_char(com, &i, &j, tab);
 		else
 			i++;
