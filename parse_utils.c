9a71d4af695e0d1ef3d7b45ab12459ec79717aec
ybesbes <ybesbes@student.42.fr>
2021-12-26 22:55:06 +0100

    parse_utils done

--- parse_utils.c
+++ parse_utils.c
@@ -6,49 +6,54 @@
 /*   By: mrahmani <mrahmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 13:03:48 by ybesbes           #+#    #+#             */
-/*   Updated: 2021/12/25 23:11:17 by mrahmani         ###   ########.fr       */
+/*   Updated: 2021/12/26 22:54:45 by ybesbes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minishell.h"
 
-void    copy_to(char *str, char *dest, int *dest_start)
+void	copy_to(char *str, char *dest, int *dest_start)
 {
-    int k;
+	int	k;
 
-    k = 0;
-    while (str[k] != '\0')
-    {
-        dest[*dest_start] = str[k++];
-        *dest_start += 1;
-    }
+	k = 0;
+	while (str[k] != '\0')
+	{
+		dest[*dest_start] = str[k++];
+		*dest_start += 1;
+	}
 }
 
-void    dollar_between_quotes(char **str, char *com, char *buff)
+void	find_last_quotes(char *com, int *i)
 {
-    int i;
-    int j;
-    int tmp;
+	while (ft_isalnum(com[*i]) == 1 && com[*i] != '"' && com[*i] != '\0')
+		*i = *i + 1;
+}
 
-    i = 0;
-    j = 0;
-    tmp = 0;
-    while (com[i] != '\0')
-    {
-        if (com[i] == '$' && is_it_between_simple_quotes(com, i) == 0
-            && ft_isspace(com[i + 1]) != 1 && com[i + 1] != '\0')
-        {
-            i++;
-            if (com[i] == '?')
-                i++;
-            else
-                while (ft_isalnum(com[i]) == 1 && com[i] != '"' && com[i] != '\0')
-                    i++;
-            if (str != NULL && str[tmp] != NULL)
-                copy_to(str[tmp++], buff, &j);
-        }
-        else
-            buff[j++] = com[i++];
-    }
-    buff[j] = '\0';
+void	dollar_between_quotes(char **str, char *com, char *buff)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	j = 0;
+	tmp = 0;
+	while (com[i] != '\0')
+	{
+		if (com[i] == '$' && is_it_between_simple_quotes(com, i) == 0
+			&& ft_isspace(com[i + 1]) != 1 && com[i + 1] != '\0')
+		{
+			i++;
+			if (com[i] == '?')
+				i++;
+			else
+				find_last_quotes(com, &i);
+			if (str != NULL && str[tmp] != NULL)
+				copy_to(str[tmp++], buff, &j);
+		}
+		else
+			buff[j++] = com[i++];
+	}
+	buff[j] = '\0';
 }
